70abb1bf3a1f483ec71b9a83468b49b09b713fdb
Ulyana <lana.arfinova@gmail.com>
2024-01-27 15:40:42 +0300

    replaced string[] and object

--- src/getUserOrderStates.ts
+++ src/getUserOrderStates.ts
@@ -8,8 +8,8 @@ const orderStates = [
 
 type OrderState = (typeof orderStates)[number]
 
-export const getUserOrderStates = (orderStates: OrderState[]): string[] => {
-    const filteredStates = [] as string[]
+export const getUserOrderStates = (orderStates: OrderState[]): OrderState[] => {
+    const filteredStates = [] as OrderState[]
     orderStates.forEach((element) => {
         if (element !== 'buyingSupplies' && element !== 'producing') {
             filteredStates.push(element)
--- src/omit.ts
+++ src/omit.ts
@@ -1,7 +1,7 @@
 export const omit = <T extends Record<any, any>, K extends keyof T>(
     obj: T,
     keyToOmit: K
-): object => {
+): Record<string, any> => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { [keyToOmit]: _, ...withoutKey } = obj
     return withoutKey
